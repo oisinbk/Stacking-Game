8493e4b4973c2ff7cf8f6a3515471e46ec52a521
oisinbk <oisinbk1@gmail.com>
2026-07-23 21:44:53 +0300

    same commit

---
+++ Assets/Scripts/Blocks/GroundCollisionDetection.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using UnityEngine;
+using GameStates;
+
+namespace Blocks
+{
+    public class GroundCollisionDetection: MonoBehaviour
+    {
+        [SerializeField] private GameOverEventChannel gameOverChannel;
+        
+        private void OnCollisionEnter(Collision other)
+        {
+            if (other.gameObject.CompareTag("Block") && gameOverChannel != null)
+            {
+                gameOverChannel.RaiseEvent(); 
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/GameStates/GameOverEventChannel.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System;
+
+namespace GameStates
+{
+    [CreateAssetMenu(fileName = "GameOverEventChannel", menuName = "Events/Game Over Event Channel")]
+    public class GameOverEventChannel : ScriptableObject
+    {
+        // Pass the block's transform or Y-position in the action payload
+        public event Action GameOver;
+
+        public void RaiseEvent()
+        {
+            GameOver?.Invoke();
+        }
+    }
+}
