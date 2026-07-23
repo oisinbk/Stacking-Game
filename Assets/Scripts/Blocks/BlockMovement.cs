bcc56bb5dbc06586b3213e3ecbf8f3e84619e230
oisinbk <oisinbk1@gmail.com>
2026-07-23 23:03:50 +0300

    added block movement controls

--- Assets/Scripts/Blocks/BlockMovement.cs
+++ Assets/Scripts/Blocks/BlockMovement.cs
@@ -1,9 +1,85 @@
 ﻿using UnityEngine;
+using UnityEngine.InputSystem;
 
 namespace Blocks
 {
+    [RequireComponent(typeof(BlockPlacement),
+        typeof(Rigidbody),
+        typeof(InputActionMap))]
+
     public class BlockMovement : MonoBehaviour
     {
+        [Header("Input Actions")]
+        [SerializeField] private InputActionReference moveAction;
+        [SerializeField] private InputActionReference rotateAction;
+
+        [Header("Physics Settings")]
+        [SerializeField] private float moveForce = 10f;
+        [SerializeField] private float torqueForce = 5f;
+
+        [Tooltip("lower the center of mass of the block by this factor")]
+        [SerializeField] private float stabilizeFactor = 0.5f;
         
+        private Rigidbody _rb;
+        private BlockPlacement _placementScript;
+
+        //inputs
+        private Vector2 _moveInput;
+        private float _rotationInput;
+
+        private void Awake()
+        {
+            _rb = GetComponent<Rigidbody>();
+            _placementScript = GetComponent<BlockPlacement>();
+        }
+
+        private void Update()
+        {
+            if (_placementScript.BlockIsStationary)
+            {
+                // Turn off the input listening for this specific block
+                moveAction.action.Disable();
+                rotateAction.action.Disable();
+                _rb.useGravity = true;
+                _rb.centerOfMass = transform.position + Vector3.down * stabilizeFactor;
+            
+                // Shut off the script so it stops taking up CPU cycles
+                enabled = false; 
+                return;
+            }
+
+            // Read directly from the Action Map reference
+            _moveInput = moveAction.action.ReadValue<Vector2>();
+            _rotationInput = rotateAction.action.ReadValue<float>();
+        }
+
+        private void FixedUpdate()
+        {
+            // Apply Y/Z movement
+            if (_moveInput != Vector2.zero)
+            {
+                Vector3 force = new Vector3(0, _moveInput.y, _moveInput.x) * moveForce;
+                _rb.AddForce(force); 
+            }
+
+            // Apply X rotation
+            if (_rotationInput != 0)
+            {
+                Vector3 torque = new Vector3(_rotationInput, 0, 0) * torqueForce;
+                _rb.AddTorque(torque);
+            }
+        }
+
+        private void OnEnable()
+        {
+            moveAction.action.Enable();
+            rotateAction.action.Enable();
+        }
+
+        private void OnDisable() 
+        {
+            moveAction.action.Disable();
+            rotateAction.action.Disable();
+        }
     }
 }
--- Assets/Scripts/Blocks/BlockPlacement.cs
+++ Assets/Scripts/Blocks/BlockPlacement.cs
@@ -7,27 +7,17 @@ namespace Blocks
     [RequireComponent(typeof(MeshCollider), typeof(Rigidbody))]
     public class BlockPlacement : MonoBehaviour, IPoolable
     {
-        [Tooltip("lower the center of mass of the block by this factor")] [SerializeField]
-        private float stabilizeFactor = 0.5f;
-
         [SerializeField] private BlockPlacingChannel blockPlacingChannel;
-
-        public static event Action GameOver;
-
-        private Rigidbody _rb;
-        private bool _stationary;
-
-        private void Awake()
-        {
-            _rb = GetComponent<Rigidbody>();
-        }
+        
+        private bool _blockIsStationary;
+        public bool BlockIsStationary => _blockIsStationary;
 
         private void OnCollisionEnter(Collision other)
         {
-            if (other.gameObject.CompareTag("Block") && !_stationary)
+            if (other.gameObject.CompareTag("Block") && !_blockIsStationary)
             {
                 BlockPlaced();
-                _stationary = true;
+                _blockIsStationary = true;
             }
         }
 
@@ -36,7 +26,6 @@ namespace Blocks
             //if player is still holding the mouse return
             //if the block is moving too much return
 
-            _rb.centerOfMass = transform.position + Vector3.down * stabilizeFactor;
             blockPlacingChannel.RaiseEvent(transform.position.y);
             //TODO: update the most top point of the block instead of the center of it
         }
